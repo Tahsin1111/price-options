5a4094f46ec92af250ac21f9dff952a53a2fd295
Tahsin1111 <entimojumder@gmail.com>
2023-09-19 22:02:51 +0600

    Responsive pricing card with Tailwind and Hero icons

--- src/components/PriceOption/PriceOption.jsx
+++ src/components/PriceOption/PriceOption.jsx
@@ -1,10 +1,22 @@
 import PropTypes from 'prop-types';
+import Feature from '../Feature/Feature';
 
 
 const PriceOption = ({option}) => {
+    const {name,price,features} = option;
     return (
-        <div>
-            
+        <div className='bg-sky-500 rounded-md p-4 flex flex-col text-white'>
+        <h2 className='text-center'>
+        <span className='text-7xl'>{price}</span>
+            <span className='text-3xl'></span>
+        </h2>
+        <h4 className='text-4xl text-center my-3'>{name}</h4>
+       <div className='pl-6 flex-grow'>
+       {
+          features.map((feature,idx) => <Feature key={idx} feature={feature}></Feature>)
+        }
+       </div>
+       <button className='mt-12 bg-green-400 w-full py-4 font-bold rounded-lg  hover:bg-yellow-300'>Buy Now</button>
         </div>
     );
 };
