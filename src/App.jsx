4a8ba4d8e8a9723d35ed9bf36f6a40ea0474a4e3
Tahsin1111 <entimojumder@gmail.com>
2023-09-19 19:20:04 +0600

    creating navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
 
   return (
     <>
-
+      {/* <DaisyNav></DaisyNav> */}
+      <Navbar></Navbar>
       <h1 className='text-5xl text-red-800'>Vite + React</h1>
 
   
